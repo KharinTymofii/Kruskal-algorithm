b801c861bd2e0fbdadc296b665a94759c2cc7134
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2024-04-21 12:51:55 +0300

    removed weight of graph

--- main.py
+++ main.py
@@ -11,15 +11,6 @@ def generate_graph(num_vertices, density):
     return random_graph.get_adjacency_matrix()
 
 
-# Функція для обчислення метрик
-def calculate_metrics(min_spanning_tree):
-    num_edges = len(min_spanning_tree)
-    total_weight = sum(weight for _, _, weight in min_spanning_tree)
-    min_weight = min(min_spanning_tree, key=lambda x: x[2])[2]
-    max_weight = max(min_spanning_tree, key=lambda x: x[2])[2]
-    return num_edges, total_weight / num_edges, min_weight, max_weight
-
-
 # Відкриваємо файл для запису результатів у форматі CSV
 with open("results.csv", "w", newline="") as csvfile:
     fieldnames = [
@@ -27,9 +18,6 @@ with open("results.csv", "w", newline="") as csvfile:
         "Vertices",
         "Time (ms)",
         "Edges",
-        "Mean Weight",
-        "Min Weight",
-        "Max Weight",
     ]
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
@@ -46,9 +34,7 @@ with open("results.csv", "w", newline="") as csvfile:
             end_time = time.time_ns()
 
             execution_time_ms = (end_time - start_time) / 1_000_000
-            num_edges, mean_weight, min_weight, max_weight = calculate_metrics(
-                min_spanning_tree
-            )
+            num_edges = len(min_spanning_tree)
 
             # Записуємо результати в файл у форматі CSV
             writer.writerow(
@@ -57,9 +43,6 @@ with open("results.csv", "w", newline="") as csvfile:
                     "Vertices": num_vertices,
                     "Time (ms)": execution_time_ms,
                     "Edges": num_edges,
-                    "Mean Weight": mean_weight,
-                    "Min Weight": min_weight,
-                    "Max Weight": max_weight,
                 }
             )
 
@@ -67,9 +50,6 @@ with open("results.csv", "w", newline="") as csvfile:
             print(f"Density: {density}, Vertices: {num_vertices}")
             print("Time (ms):", execution_time_ms)
             print("Edges:", num_edges)
-            print("Mean Weight:", mean_weight)
-            print("Min Weight:", min_weight)
-            print("Max Weight:", max_weight)
             print()
 
 print("Results saved to results.csv")
