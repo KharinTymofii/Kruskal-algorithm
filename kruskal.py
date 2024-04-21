4acc00bf4c734e63490852a25390fbec15e0896c
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2024-04-21 18:24:26 +0300

    added finding mst from adjacency_list

--- kruskal.py
+++ kruskal.py
@@ -1,20 +1,7 @@
 class Kruskal:
-    def __init__(self, graph):
-        self.graph = graph
-
-    def get_min_spanning_tree(self):
-        num_nodes = len(self.graph)
+    def get_mst_from_edges(edges):
         included_nodes = []  # Вершини, які вже включені в minimal spanning tree
         minimal_spanning_tree = []  # Початковий мінімальний minimal spanning tree
-
-        # Сортуємо ребра графа за зростанням ваги
-        edges = []
-        for i in range(num_nodes):
-            for j in range(i + 1, num_nodes):
-                if self.graph[i][j] != 0:
-                    edges.append((i, j, self.graph[i][j]))
-
-        edges.sort(key=lambda x: x[2])
 
         for edge in edges:
             i, j, weight = edge
@@ -24,8 +11,38 @@ class Kruskal:
             ):  # Перевіряємо, чи вершини i та j вже включені в minimal_spanning_tree
                 continue
 
-            minimal_spanning_tree.append((i, j, weight))  # Додаємо ребро до minimal_spanning_tree
+            minimal_spanning_tree.append(
+                (i, j, weight)
+            )  # Додаємо ребро до minimal_spanning_tree
             included_nodes.append(i)  # Додаємо вершину i до включених вершин
             included_nodes.append(j)  # Додаємо вершину j до включених вершин
 
         return minimal_spanning_tree  # Повертаємо мінімальний minimal spanning tree
+
+    def get_mst_from_adjacency_matrix(adjacency_matrix):
+        num_nodes = len(adjacency_matrix)
+
+        # Створюємо список ребер графа
+        
+        edges = []
+        for i in range(num_nodes):
+            for j in range(i + 1, num_nodes):
+                if adjacency_matrix[i][j] != 0:
+                    edges.append((i, j, adjacency_matrix[i][j]))
+
+        # Сортуємо ребра графа за зростанням ваги
+        edges.sort(key=lambda x: x[2])
+
+        return Kruskal.get_mst_from_edges(edges)
+
+    def get_mts_from_adjacency_list(adjacency_list):
+        # Створюємо список ребер графа
+        edges = []
+        for node, adjacents in adjacency_list.items():
+            for adjacent, weight in adjacents.items():
+                edges.append((node, adjacent, weight))
+
+        # Сортуємо ребра графа за зростанням ваги
+        edges.sort(key=lambda x: x[2])
+
+        return Kruskal.get_mst_from_edges(edges)
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import csv
 def generate_graph(num_vertices, density):
     random_graph = Graph()
     random_graph.generate_random_graph(num_vertices, density)
-    return random_graph.get_adjacency_matrix()
+    return random_graph
 
 
 # Відкриваємо файл для запису результатів у форматі CSV
@@ -16,7 +16,8 @@ with open("results.csv", "w", newline="") as csvfile:
     fieldnames = [
         "Density",
         "Vertices",
-        "Avg Time (ms)",
+        "Avg Time from matrix (ms)",
+        "Avg Time from list (ms)",
         "Avg Edges",
     ]
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
@@ -24,28 +25,45 @@ with open("results.csv", "w", newline="") as csvfile:
     writer.writeheader()
 
     # Генеруємо графи та рахуємо метрики
-    for density in [round(0.5 + 0.1 * i, 1) for i in range(6)]:
+    for density in [round(0.5 + 0.1 * i, 1) for i in range(5)]:
         for num_vertices in range(20, 201, 20):
-            total_time_ms = 0
+            total_time_matrix_ms = 0
+            total_time_adjacency_list_ms = 0
             total_edges = 0
 
             # Виконуємо алгоритм п'ять разів
             for _ in range(5):
-                adjacency_matrix = generate_graph(num_vertices, density)
+                graph = generate_graph(num_vertices, density)
 
-                start_time = time.time_ns()
-                kruskal_finder = Kruskal(adjacency_matrix)
-                min_spanning_tree = kruskal_finder.get_min_spanning_tree()
-                end_time = time.time_ns()
+                start_time_adjacency_list = time.time_ns()
+                min_spanning_tree_adjacency_list = Kruskal.get_mts_from_adjacency_list(
+                    graph.get_adjacency_list()
+                )
+                end_time_adjacency_list = time.time_ns()
+                start_time_matrix = time.time_ns()
+                min_spanning_tree_matrix = Kruskal.get_mst_from_adjacency_matrix(
+                    graph.get_adjacency_matrix()
+                )
+                end_time_matrix = time.time_ns()
 
-                execution_time_ms = (end_time - start_time) / 1_000_000
-                num_edges = len(min_spanning_tree)
+                execution_matrix_time_ms = (
+                    end_time_matrix - start_time_matrix
+                ) / 1_000_000
 
-                total_time_ms += execution_time_ms
+                total_time_matrix_ms += execution_matrix_time_ms
+
+
+                execution_adjacency_list_time_ms = (
+                    end_time_adjacency_list - start_time_adjacency_list
+                ) / 1_000_000
+                total_time_adjacency_list_ms += execution_adjacency_list_time_ms
+
+                num_edges = len(min_spanning_tree_matrix)
                 total_edges += num_edges
 
             # Беремо середнє значення часу виконання та кількості ребер
-            avg_time_ms = total_time_ms / 5
+            avg_time_matrix_ms = total_time_matrix_ms / 5
+            avg_time_adjacency_list_ms = total_time_adjacency_list_ms / 5
             avg_edges = total_edges / 5
 
             # Записуємо результати в файл у форматі CSV
@@ -53,14 +71,16 @@ with open("results.csv", "w", newline="") as csvfile:
                 {
                     "Density": density,
                     "Vertices": num_vertices,
-                    "Avg Time (ms)": avg_time_ms,
+                    "Avg Time from matrix (ms)": avg_time_matrix_ms,
+                    "Avg Time from list (ms)": avg_time_adjacency_list_ms,
                     "Avg Edges": avg_edges,
                 }
             )
 
             # Виводимо результати у консоль
             print(f"Density: {density}, Vertices: {num_vertices}")
-            print("Avg Time (ms):", avg_time_ms)
+            print("Avg Time from matrix (ms):", avg_time_matrix_ms)
+            print("Avg Time from list (ms):", avg_time_adjacency_list_ms)
             print("Avg Edges:", avg_edges)
             print()
 
